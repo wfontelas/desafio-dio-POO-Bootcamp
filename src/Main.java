5cd23ae70f641637a658cf39d114a2a89c7d41bd
William Fontelas <williamdevsuporte@gmail.com>
2024-07-18 04:06:25 -0300

    Criando as classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+
+        curso.setTitulo("curso Java");
+        curso.setDescricao("Descrição curso java");
+        curso.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+
+        System.out.println(curso);
+
+        System.out.println(mentoria);
+
     }
 }
